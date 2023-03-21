1123403fbbe2770902df910a95f12d34abe69e9f
obuyajr <felix37brock@gmail.com>
2023-03-21 22:55:05 +0300

    1st commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# main.py
+import nltk
+from flask import request
+from flask import jsonify
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['text']
+    nltk.download('vader_lexicon')
+    from nltk.sentiment.vader import SentimentIntensityAnalyzer
+    sid = SentimentIntensityAnalyzer()
+    score = ((sid.polarity_scores(str(text))))['compound']
+
+    if(score > 0):
+        label = 'Positive'
+    elif(score == 0):
+        label = 'Neutral'
+    else:
+        label = 'Negative'
+
+    return(render_template('index.html', variable=label))
+
+if __name__ == "__main__":
+    app.run(port='8088', threaded=False, debug=True)
